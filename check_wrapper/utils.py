1bb0f3bd4de1b7655dd1b070fc321f4c612dca5a
anton <anton@zinchenko.pro>
2024-08-27 16:34:43 +0300

    Removed the test plug

--- check_wrapper/utils.py
+++ check_wrapper/utils.py
@@ -17,17 +17,17 @@ async def get_check_data(ai_wrapper: AiWrapper, check_text: str) -> dict:
     return check_data
 
 def get_check_json(check_credentials: dict) -> dict:
-    with open('/home/anton/Desktop/Cost_accounting_bot/check.json', 'r') as file:
-        check = file.read()
-    return json.loads(check)
-    # resp = requests.post(url=getenv("PROVERKACHEKA_BASE_URL"),
-    #                      json={"token": getenv("PROVERKACHEKA_API_TOKEN"),
-    #                            "fn": check_credentials['fn'],
-    #                            "fd": check_credentials["fd"],
-    #                            "fp": check_credentials["fp"],
-    #                            "t": check_credentials["t"],
-    #                            "s": check_credentials["s"]})
-    # return json.loads(resp.content.decode())
+    # with open('/home/anton/Desktop/Cost_accounting_bot/check.json', 'r') as file:
+    #     check = file.read()
+    # return json.loads(check)
+    resp = requests.post(url=getenv("PROVERKACHEKA_BASE_URL"),
+                         json={"token": getenv("PROVERKACHEKA_API_TOKEN"),
+                               "fn": check_credentials['fn'],
+                               "fd": check_credentials["fd"],
+                               "fp": check_credentials["fp"],
+                               "t": check_credentials["t"],
+                               "s": check_credentials["s"]})
+    return json.loads(resp.content.decode())
 
 def get_important_check_data(check_data: dict) -> dict:
     important_data = {}
