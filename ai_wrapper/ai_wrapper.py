1e723248ba2676bca489fda7de7a70de97826e59
Anton <a@a.com>
2025-03-16 20:02:12 +0300

    Updated ai_wrapper

--- ai_wrapper/ai_wrapper.py
+++ ai_wrapper/ai_wrapper.py
@@ -4,6 +4,8 @@ from typing import Iterable
 from openai import AsyncOpenAI
 from openai.types.chat import ChatCompletionMessageParam
 
+from llama_cpp import Llama
+
 from ai_wrapper.utils import (get_input_for_categories_user_prompt,
                               get_input_for_types_user_prompt,
                               parsing_model_json_response)
@@ -14,32 +16,44 @@ from datafiles import (GET_CATEGORIES_FOR_CHECK_SYSTEM_PROMPT,
 
 class AiWrapper:
     def __init__(self):
-        self.api_key = getenv("OPENAI_API_KEY")
-        assert self.api_key is not None, "Set enviroment variable 'OPENAI_API_KEY'"
-        self.base_url = getenv("OPENAI_BASE_URL")
-        assert self.api_key is not None, "Set enviroment variable 'OPENAI_BASE_URL'"
-        self.model = getenv("OPENAI_MODEL")
-        assert self.api_key is not None, "Set enviroment variable 'OPENAI_MODEL'"
-        self.temperature = 0.2
+        # self.api_key = getenv("OPENAI_API_KEY")
+        # assert self.api_key is not None, "Set enviroment variable 'OPENAI_API_KEY'"
+        # self.base_url = getenv("OPENAI_BASE_URL")
+        # assert self.api_key is not None, "Set enviroment variable 'OPENAI_BASE_URL'"
+        # self.model = getenv("OPENAI_MODEL")
+        # assert self.api_key is not None, "Set enviroment variable 'OPENAI_MODEL'"
+        # self.temperature = 0.2
 
-        self.client = AsyncOpenAI(api_key=self.api_key, base_url=self.base_url)
+        # self.client = AsyncOpenAI(api_key=self.api_key, base_url=self.base_url)
+
+        # /home/user/Cost_accounting_bot_with_checks/ai_wrapper/models/nemo12b.gguf
+        self.client = Llama(model_path="ai_wrapper/models/nemo12b.gguf", device="gpu",
+                            n_gpu_layers=1111, n_ctx=1024, verbose=False)
 
     async def create(self, messages: Iterable[ChatCompletionMessageParam]):
-        response = await self.client.chat.completions.create(
-            messages=messages,
-            model=self.model,
-            temperature=self.temperature,
-            n=1,
-            response_format={"type": "json_object"},
-            max_tokens=4096,
-        )
-        content = response.choices[0].message.content
-        assert content is not None
-        return parsing_model_json_response(content)
+        # response = await self.client.chat.completions.create(
+        #     messages=messages,
+        #     model=self.model,
+        #     temperature=self.temperature,
+        #     n=1,
+        #     response_format={"type": "json_object"},
+        #     max_tokens=4096,
+        # )
+        # content = response.choices[0].message.content
+        # assert content is not None
+        # return parsing_model_json_response(content)
+        answer = (self.client.create_chat_completion(
+            response_format={
+                "type": "json_object"
+            },
+            messages=messages
+        ))
 
     async def invoke(self, system_prompt: str, user_prompt: str):
-        system_content = [{"type": "text", "text": system_prompt}]
-        user_content = [{"type": "text", "text": user_prompt}]
+        # system_content = [{"type": "text", "text": system_prompt}]
+        # user_content = [{"type": "text", "text": user_prompt}]
+        system_content = system_prompt
+        user_content = user_prompt
 
         messages = [
             {"role": "system", "content": system_content},
